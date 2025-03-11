ac5332f3156981472ffae1a79601ba589989aa82
Tyler Erickson <tyler@vorgeo.com>
2025-03-10 20:04:17 -0700

    simplify the postCreateCommand path

--- .devcontainer/tutorial_1_container/postCreateCommand.sh
+++ .devcontainer/tutorial_1_container/postCreateCommand.sh
@@ -4,8 +4,5 @@ curl -fsSL https://pixi.sh/install.sh | bash
 # Install conda and pypi packages with pixi
 /home/vscode/.pixi/bin/pixi install
 
-# # Display information on the pixi environment
-# /home/vscode/.pixi/bin/pixi info
-
 # Install a Jupyter kernel for the devcontainer
 /home/vscode/.pixi/bin/pixi run python -m ipykernel install --user --name=tutorial-1-kernel --display-name 'Tutorial 1 Kernel'
