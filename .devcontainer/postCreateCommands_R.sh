fa25208798209fe2e9ae2758d2a81bf31f0c83df
Tyler Erickson <tyler@vorgeo.com>
2025-03-20 18:45:54 -0700

    Initial version of working R dev container

--- .devcontainer/postCreateCommands_R.sh
+++ .devcontainer/postCreateCommands_R.sh
@@ -7,9 +7,12 @@ echo "--------------------------------"
 echo "$HOME/.pixi/bin/pixi install -v"
 $HOME/.pixi/bin/pixi install -v
 
+# echo "--------------------------------"
+# $HOME/.pixi/bin/pixi run R -e "install.packages(\"units\", repos = \"https://cloud.r-project.org/\")"
 echo "--------------------------------"
-echo "SERVICE_NAME=$SERVICE_NAME"
-echo "CONTAINER_NAME=$CONTAINER_NAME"
-
+$HOME/.pixi/bin/pixi run R -e "install.packages(\"IRkernel\", repos = \"https://cloud.r-project.org/\")"
 echo "--------------------------------"
 $HOME/.pixi/bin/pixi run R -e "IRkernel::installspec(name = \"$SERVICE_NAME\", displayname = \"$CONTAINER_NAME\")"
+
+echo "--------------------------------"
+$HOME/.pixi/bin/pixi run jupyter kernelspec list
